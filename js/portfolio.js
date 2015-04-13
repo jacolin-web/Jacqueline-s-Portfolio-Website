53cc7cbd6de0466aceeffab2677f02c12d597550
Jacqueline L <Jacque3L@verizon.net>
2015-04-13 10:45:55 -0400

    v1

---
+++ js/portfolio.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$('.fade').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  swipe: true,
+  accessibility: true,
+  dragability: true,
+});
+});
